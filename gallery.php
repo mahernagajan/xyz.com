4cbc2b6a0782daedd27fbd972d79cd4b8647cfb6
mahernagajan <145439216+mahernagajan@users.noreply.github.com>
2023-09-19 14:28:42 +0530

    Add files via upload

---
+++ gallery.php
@@ -0,0 +1,27 @@
+<link rel="stylesheet" type="text/css" href="style.css">
+<p style="background-color:white;"><br><br></p>
+<div class="menu">
+	<br>
+	<ul class="menu1">
+			<a href="home.php" style="color: white; font-size:30px;">Home</a>
+			<a href="gallery.php" style="color: white; font-size:30px;">Gallery</a>
+			<a href="" style="color: white; font-size:30px;">About_Us</a>
+			<a href="" style="color: white; font-size:30px;">Services</a>
+	</ul>
+	<br>
+</div>
+<br>
+<br>
+<img src=".\hot-n-spice-mangnath-road-junagadh-fast-food-n8l4t2pj1w-250.jpg" style="width:100vh; height:600px;">
+<img src=".\hot-n-spice-mangnath-road-junagadh-fast-food-qsbyj9f9c8-250.jpg"
+style="width: 100vh; height: 600px;">
+<br>
+<br>
+<img src=".\hot-n-spice-mangnath-road-junagadh-fast-food-suqckq8g1r-250.jpg"
+style="width: 100vh; height: 400px;">
+<img src=".\hot-n-spice-mangnath-road-junagadh-fast-food-v9f6f9i7m7-250.jpg"
+style="width: 100vh; height: 400px;">
+<br><br>
+<div class="footer"><br>
+	<div>Design & Developed By:Nagajan Thapaliya</div>
+</div>
---
+++ home.php
@@ -0,0 +1,28 @@
+<link rel="stylesheet" type="text/css" href="style.css">
+<div style="background-color:white;"><br><br><br></div>
+<p>
+	<img src=".\images (3).jpg" style="border-radius: 160px">
+		<h1 style="margin-top:-140px;">
+				<span class="margin" style="margin-left: 305px;">&nbsp;Kathiyawadi  Resturant&nbsp;</span>
+		</h1>
+		<img src=".\images (4).jpg" style="margin-left: 720px; margin-top: -60px;">
+		<img src=".\download (4).jpg" style="margin-left: 970px; margin-top: -226px;">
+</p>
+<br><br><br>
+<p style="background-color:white;"><br></p>
+<div class="menu">
+	<br>
+	<ul class="menu1">
+			<a href="home.php" style="color: white; font-size:30px;">Home</a>
+			<a href="gallery.php" style="color: white; font-size:30px;">Gallery</a>
+			<a href="" style="color: white; font-size:30px;">About_Us</a>
+			<a href="" style="color: white; font-size:30px;">Services</a>
+	</ul>
+	<br>
+</div>
+<br><br>
+<img class="maindiv">
+<br><br>
+<div class="footer"><br>
+	<div>Design & Developed By:Nagajan Thapaliya</div>
+</div>
